bc4903b74e29b8b920cde0cd92354a06b9d5f770
ank809 <ankita51523@gmail.com>
2023-06-26 01:20:44 +0530

    changes in home page

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,3 @@
-import 'dart:html';
 
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -6,6 +5,7 @@ import 'icon_content.dart';
 import 'constants.dart';
 import 'reuseable_code.dart';
 enum Gendertype {male, female}
+const bottomColor=Color(0xFFEB1555);
 
 class Home_page extends StatefulWidget {
   const Home_page({super.key,});
@@ -18,166 +18,216 @@ class _Home_pageState extends State<Home_page> {
     Gendertype ?selectedGender;
     int height=180;
     int weight=60;
+    int age=10;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('BMI Calculator'),),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Expanded(child: 
-            Row(
-              children: [
-                Expanded(child: 
-                GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      selectedGender=Gendertype.male;
-                    });
-                  },
-                  child: Reuseable_container(
-                    colour: selectedGender==Gendertype.male? kactiveCardColor:kinactiveCardColor,
-                    cardChild: IconContent(
-                      icon_child:FontAwesomeIcons.mars,
-                      child_string: 'MALE',
-                    ),
-                  ),
-                ),
-                ),
-                Expanded(child: 
-                GestureDetector(
-                  onTap: (){
-                    setState(() {
-                       selectedGender=Gendertype.female;
-                    }); 
-                  },
-                  child: GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        selectedGender=Gendertype.female;
-                      });
-                    },
-                    child: Reuseable_container(
-                      colour: selectedGender==Gendertype.female? kactiveCardColor:kinactiveCardColor,
-                      cardChild: IconContent(
-                        icon_child: FontAwesomeIcons.venus ,
-                        child_string: 'FEMALE',
-                      ),
-                    ),
-                  ),
-                ),
-                ),
-              ],
-            ),
-            ),
-            Expanded(child: 
-            Reuseable_container(
-              colour: kactiveCardColor,
-              cardChild: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+      body:  SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width, 
+        child: RepaintBoundary(
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  Text('HEIGHT',
-                  style: klabelText,),
+                  Expanded(child: 
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.baseline,
-                    textBaseline: TextBaseline.alphabetic,
                     children: [
-                      Text(height.toString(), 
-                      style:ktextStyle,),
-                      Text('cm',
-                      style: klabelText,)
+                      Expanded(child: 
+                      GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            selectedGender=Gendertype.male;
+                          });
+                        },
+                        child: Reuseable_container(
+                          colour: selectedGender==Gendertype.male? kactiveCardColor:kinactiveCardColor,
+                          cardChild: IconContent(
+                            icon_child:FontAwesomeIcons.mars,
+                            child_string: 'MALE',
+                          ),
+                        ),
+                      ),
+                      ),
+                      Expanded(child: 
+                      GestureDetector(
+                        onTap: (){
+                          setState(() {
+                             selectedGender=Gendertype.female;
+                          }); 
+                        },
+                        child: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              selectedGender=Gendertype.female;
+                            });
+                          },
+                          child: Reuseable_container(
+                            colour: selectedGender==Gendertype.female? kactiveCardColor:kinactiveCardColor,
+                            cardChild: IconContent(
+                              icon_child: FontAwesomeIcons.venus ,
+                              child_string: 'FEMALE',
+                            ),
+                          ),
+                        ),
+                      ),
+                      ),
                     ],
                   ),
-                  Slider(value: height.toDouble(),
-                  min: 120.0,
-                  max: 220.0, 
-                  activeColor: Color(0xFFEB1555),
-                  inactiveColor: Color(0xFF8D8E98),
-                  onChanged: (double newValue){
-                    setState(() {
-                      height=newValue.toInt();
-                    });
-                  }),
-                ],
-                ),
                   ),
-            ),
-            Expanded(child: 
-            Row(
-              children: [
-                Expanded(child: 
-                Reuseable_container(
-                  colour:kactiveCardColor,
-                  cardChild: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text('WEIGHT',
-                      style: ktextStyle,),
-                      Text(weight.toString(),
-                      style: ktextStyle,),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          RoundIconButton(
-                          icon : FontAwesomeIcons.plus, 
-                          onPressed: (){
-                            setState(() {
-                              weight--;
-                            });
-                          },),
-                          SizedBox(width: 10.0),
-                          RoundIconButton(
-                            icon: FontAwesomeIcons.minus,
-                            onPressed: (){
-                              setState(() {
-                                weight++;
-                              });
-                            },),
-                        ],
+                  Expanded(child: 
+                  Reuseable_container(
+                    colour: kactiveCardColor,
+                    cardChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('HEIGHT',
+                        style: klabelText,),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.baseline,
+                          textBaseline: TextBaseline.alphabetic,
+                          children: [
+                            Text(height.toString(), 
+                            style:ktextStyle,),
+                            Text('cm',
+                            style: klabelText,)
+                          ],
+                        ),
+                        Slider(value: height.toDouble(),
+                        min: 120.0,
+                        max: 220.0, 
+                        activeColor:bottomColor ,
+                        inactiveColor: Color(0xFF8D8E98),
+                        onChanged: (double newValue){
+                          setState(() {
+                            height=newValue.toInt();
+                          });
+                        }),
+                      ],
                       ),
-                    ]),
+                        ),
                   ),
-                ),
-                Expanded(child: 
-                Reuseable_container(
-                  colour: kactiveCardColor,
+                  Expanded(child: 
+                  Row(
+                    children: [
+                      Expanded(child: 
+                      Reuseable_container(
+                        colour:kactiveCardColor,
+                        cardChild:  Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text('WEIGHT',
+                              style: klabelText,),
+                              Text(weight.toString(),
+                              style: ktextStyle,),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  RoundIconButton(
+                                  icon : FontAwesomeIcons.plus, 
+                                  onPressed: (){
+                                    setState(() {
+                                      weight++;
+                                    });
+                                  },),
+                                  SizedBox(width: 10.0),
+                                  RoundIconButton(
+                                    icon: FontAwesomeIcons.minus,
+                                    onPressed: (){
+                                      setState(() {
+                                        weight--;
+                                      });
+                                    },),
+                                ],
+                              ),
+                            ]),
+                        ),
+                      ),
+                      Expanded(child: 
+                      Reuseable_container(
+                        colour: kactiveCardColor,
+                        cardChild: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('AGE', 
+                            style: klabelText,),
+                            Text(age.toString(), 
+                            style: ktextStyle,),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                RoundIconButton(icon: FontAwesomeIcons.plus, 
+                                onPressed: (){
+                                  setState(() {
+                                    age++;
+                                  });
+                                }),
+                                SizedBox(width: 10.0,),
+                                RoundIconButton(icon: FontAwesomeIcons.minus, 
+                                onPressed: (){
+                                  setState(() {
+                                    age--;
+                                  });
+                                })
+                              ],),
+                          ],
+                        ),
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-              ],
-            ),
-            ),
-             Container(
-                width: double.infinity,
-                color: kbottomContainerColor,
-                margin: EdgeInsets.only(top: 10.0),
-                height: kbottomcolor,
-              ),
-          ]
-            ),
-      )
-          );
+                  ),
+                   Container(
+                      width: double.infinity,
+                      
+                      margin: EdgeInsets.only(top: 10.0),
+                      height: kbottomcolor,
+                      child: ElevatedButton(
+                        child: Text('CALCULATE ', 
+                        style:TextStyle(
+                          fontWeight: FontWeight.w900,
+                          fontSize: 30.0,
+                          color: Colors.white)
+                           ,) , 
+                        onPressed: (){},
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStatePropertyAll(bottomColor),
+                        ),
+                        ), 
+                    ),
+                ]
+                ),                  
+          ),
+        ),
+      ),
+    );
   }
 }
 class RoundIconButton extends StatelessWidget {
-  RoundIconButton({required this.icon,   required this.onPressed});
-final IconData icon;
-final Function onPressed;
+  RoundIconButton({required this.icon, required this.onPressed});
+
+  final IconData icon;
+  final Function()? onPressed;
+
   @override
   Widget build(BuildContext context) {
-    return RawMaterialButton(onPressed: onPressed(),
-    child: Icon(icon),
-    elevation: 6.0,
-    shape: CircleBorder(),
-    constraints: BoxConstraints.tightFor(
-      width: 56.0,
-      height: 56.0,
-      
-    ),
-    fillColor: Color(0xFF4C4F5E),
-    ) ;
+    return RawMaterialButton(
+      onPressed: onPressed,
+      child: Icon(icon),
+      elevation: 6.0,
+      shape: CircleBorder(),
+      constraints: BoxConstraints.tightFor(
+        width: 56.0,
+        height: 56.0,
+      ),
+      fillColor: Color(0xFF4C4F5E),
+    );
   }
 }
+
+
+
 
 
